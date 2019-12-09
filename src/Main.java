86a7ae0febfdecbbabff659674051558c66e883e
jnfrle <jennifer.lee1@montgomerycollege.edu>
2019-12-09 10:30:32 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        String myVar = "Jen";
+
+        System.out.println("Hello World!");
+        System.out.println(myVar);
+        System.out.println("Hello and welcome, " + myVar);
+    }
+}
